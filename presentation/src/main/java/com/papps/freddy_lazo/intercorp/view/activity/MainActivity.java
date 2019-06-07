313b33f75e316e431cce3966c0acbb03b41b5d71
Freddy <flazo@peuapps.com.pe>
2019-06-06 22:38:45 -0500

    Initial commit

---
+++ presentation/src/main/java/com/papps/freddy_lazo/intercorp/view/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.papps.freddy_lazo.intercorp.view.activity;
+
+import android.os.Bundle;
+
+import com.papps.freddy_lazo.intercorp.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
